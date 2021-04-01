3255787c032c96db64ad298f56de05354ee48626
idan506 <58820156+idan506@users.noreply.github.com>
2021-04-01 16:44:12 +0700

    new

---
+++ euruka/euruka/src/test/java/com/example/euruka/EurukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.euruka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
